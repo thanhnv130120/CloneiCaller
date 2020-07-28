ac3ec91006a25504ba78f27d5b05356784273875
Nguyen An Thiet <thietna@hiworld.com.vn>
2020-07-28 18:39:41 +0700

    [Detail Activity.java] done

--- app/src/main/java/com/example/cloneicaller/DetailDiaryActivity.java
+++ app/src/main/java/com/example/cloneicaller/DetailDiaryActivity.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class DetailDiaryActivity extends AppCompatActivity {
+public class DetailDiaryActivity extends AppCompatActivity implements View.OnClickListener {
     private TextView tvNameDetail;
     private TextView tvPhoneDetail;
     private TextView tvPhoneNumberDetail;
+    private ImageView imgBtnBack;
     @Override
     protected void onStart() {
         super.onStart();
@@ -28,5 +31,16 @@ public class DetailDiaryActivity extends AppCompatActivity {
         tvNameDetail = findViewById(R.id.tv_name_detail);
         tvPhoneDetail = findViewById(R.id.tv_phone_detail);
         tvPhoneNumberDetail = findViewById(R.id.tv_phone_number_detail);
+        imgBtnBack = findViewById(R.id.img_btn_back_detail);
+        imgBtnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.img_btn_back_detail:
+                finish();
+                break;
+        }
     }
 }
